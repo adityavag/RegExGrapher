47c5d40136f8e1df54a9b8bdbaa73bfcf5209989
adi247u <adi247u@gmail.com>
2023-10-23 15:28:54 +0530

    feat: Added Viz.zs

--- JS/Automata.js
+++ JS/Automata.js
@@ -1,12 +1,12 @@
 const { toDotScript } = require("automata.js/src/dot-converter");
-
+const { instance } = require("@viz-js/viz");
 var dfa = dfaParser.parseToDFA();
 var nfa = nfaParser.parseToNFA();
 
 
 $('.dfaResult').html(
     `<p>Type: ${dfa['type']}</p>
-    <p>Accept States: ${dfa['acceptStates']}</p>
+    <!-- <p>Accept States: ${dfa['acceptStates']}</p> -->
     <p>Intial State: ${dfa['initialState']}</p>
     <p>Number of States: ${dfa['numOfStates']}</p>
     <!-- <p>Type: ${dfa['type']}</p>
@@ -14,11 +14,18 @@ $('.dfaResult').html(
 );
 $('.nfaResult').html(
     `<p>Type: ${nfa['type']}</p>
-    <p>Accept States: $ndfa['acceptStates']}</p>
+    <!-- <p>Accept States: $ndfa['acceptStates']}</p> -->
     <p>Intial State: ${nfa['initialState']}</p>
     <p>Number of States: ${nfa['numOfStates']}</p>
     <!-- <p>Type: ${nfa['type']}</p>
     <p>Type: ${nfa['type']}</p> -->`
 )
-console.log(dfa, nfa);
+
+instance().then(viz => {
+    $("#dfa-graph-container").append(viz.renderSVGElement(dfa.toDotScript()));
+    $("#nfa-graph-container").append(viz.renderSVGElement(nfa.toDotScript()));
+});
+
+console.log(dfa, nfa)
+console.log(dfa.toDotScript());
 
