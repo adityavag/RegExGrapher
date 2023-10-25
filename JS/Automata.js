9da976c4244dce1ea765c329e4a8f48fdc09aa4c
Stealthy-CodeWalker <webdev.vanguard@gmail.com>
2023-10-25 22:01:57 +0530

    Issue Rsolved

--- JS/Automata.js
+++ JS/Automata.js
@@ -1,28 +1,41 @@
-var dfa = dfaParser.parseToDFA();
-var nfa = nfaParser.parseToNFA();
+const regParser = require('automata.js');
 
-
-$('.dfaResult').html(
-    `<p>Type: ${dfa['type']}</p>
-    <!-- <p>Accept States: ${dfa['acceptStates']}</p> -->
-    <p>Intial State: ${dfa['initialState']}</p>
-    <p>Number of States: ${dfa['numOfStates']}</p>
-    <!-- <p>Type: ${dfa['type']}</p>
-    <p>Type: ${dfa['type']}</p> -->`
-);
-$('.nfaResult').html(
-    `<p>Type: ${nfa['type']}</p>
-    <!-- <p>Accept States: $ndfa['acceptStates']}</p> -->
-    <p>Intial State: ${nfa['initialState']}</p>
-    <p>Number of States: ${nfa['numOfStates']}</p>
-    <!-- <p>Type: ${nfa['type']}</p>
-    <p>Type: ${nfa['type']}</p> -->`
-)
-
-instance().then(viz => {
-    $("#dfa-graph-container").append(viz.renderSVGElement(dfa.toDotScript()));
-    $("#nfa-graph-container").append(viz.renderSVGElement(nfa.toDotScript()));
+$('#compile').on('click', () => {
+    // $('.dfaResult').remove();
+    // $('.nfaResult').remove();
+    // $("#dfa-graph-container").remove();
+    // $("#nfa-graph-container").remove();
+    regExString = regExInput.val();
+    dfaParser = new regParser.RegParser(`${regExString}`);
+    nfaParser = new regParser.RegParser(`${regExString}`);
+    console.log(regExString);
+    console.log(typeof (regExString));
+    dfa = dfaParser.parseToDFA();
+    nfa = nfaParser.parseToNFA();
+    console.log(typeof(regExString));
+    $('.dfaResult').html(
+        `<p>Type: ${dfa['type']}</p>
+        <!-- <p>Accept States: ${dfa['acceptStates']}</p> -->
+        <p>Intial State: ${dfa['initialState']}</p>
+        <p>Number of States: ${dfa['numOfStates']}</p>
+        <!-- <p>Type: ${dfa['type']}</p>
+        <p>Type: ${dfa['type']}</p> -->`
+    );
+    $('.nfaResult').html(
+        `<p>Type: ${nfa['type']}</p>
+        <!-- <p>Accept States: $ndfa['acceptStates']}</p> -->
+        <p>Intial State: ${nfa['initialState']}</p>
+        <p>Number of S  tates: ${nfa['numOfStates']}</p>
+        <!-- <p>Type: ${nfa['type']}</p>
+        <p>Type: ${nfa['type']}</p> -->`
+    )
+    
+    instance().then(viz => {
+        $("#dfa-graph-container").html(viz.renderSVGElement(dfa.toDotScript()));
+        $("#nfa-graph-container").html(viz.renderSVGElement(nfa.toDotScript()));
+    });
 });
+
 
 // console.log(dfa)
 // console.log(nfa)
--- JS/GlobalVariables.js
+++ JS/GlobalVariables.js
@@ -1,15 +1,6 @@
-const regParser = require('automata.js');
 const { toDotScript } = require("automata.js/src/dot-converter");
 const { instance } = require("@viz-js/viz");
 
 var regExInput = $(".regExInput");
-let regExString;
-
-$('#compile').on('click', () => {
-    regExString = regExInput.val();
-    console.log(regExString);
-    console.log(typeof (regExString));
-});
-
-var dfaParser = new regParser.RegParser('(a+b)*');
-var nfaParser = new regParser.RegParser(`${regExString}`);
+var regExString = '';
+var dfaParser, nfaParser, dfa, nfa;
